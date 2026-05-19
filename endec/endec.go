4cdb41e2c86756e10e36e41d42c385a4bb1270e8
Mahboob Hasan <hasanmahboob059@gmail.com>
2026-05-20 02:11:40 +0530

    feat: basic manual encoder and decoder

---
+++ endec/endec.go
@@ -0,0 +1,88 @@
+package endec
+
+import (
+	"bytes"
+)
+
+type Person struct {
+	Name string
+	Id   int32
+}
+
+type Field struct {
+	FieldName   string
+	FieldSeqNum int
+	FieldType   string
+}
+
+type ProtoStruct map[string][]Field
+
+func Decoder(arr []byte) Person {
+	i := 0
+	person := Person{}
+	for i < len(arr) {
+		num := arr[i]
+
+		buffType := num & 0b111
+		// buffTag := num >> 3
+
+		switch buffType {
+		case 2:
+			length := int(arr[i+1])
+			stringArr := arr[i+2 : i+2+length]
+			str := string(stringArr)
+			person.Name = str
+			i = i + 2 + length
+			break
+		case 0:
+			bit := 0
+			num := 0
+			for arr[i+1]>>7 != 0 {
+				curr := int(arr[i+1]) & 0x7F
+				num = num + curr<<bit
+
+				bit += 7
+				i++
+
+			}
+
+			curr := int(arr[i+1]) & 0x7F
+			num = num + curr<<bit
+			bit += 7
+			i = i + 2
+			person.Id = int32(num)
+
+			break
+
+		default:
+
+		}
+
+	}
+
+	return person
+
+}
+
+func Encoder(user Person, p ProtoStruct) []byte {
+	fields := p["Person"]
+
+	var buf bytes.Buffer
+
+	for _, field := range fields {
+		switch field.FieldName {
+
+		case "name":
+			buf.WriteByte(byte(field.FieldSeqNum<<3 | 2))
+			nameBytes := []byte(user.Name)
+			buf.WriteByte(byte(len(nameBytes)))
+			buf.Write(nameBytes)
+
+		case "id":
+			buf.WriteByte(byte(field.FieldSeqNum<<3 | 0))
+			buf.WriteByte(byte(user.Id))
+		}
+	}
+
+	return buf.Bytes()
+}
--- main.go
+++ main.go
@@ -1,94 +1,38 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"protobuffs/endec"
 )
-
-type Person1 struct {
-	UserName       string   `json:"user_name"`
-	FavoriteNumber int      `json:"favorite_number"`
-	Interests      []string `json:"interests"`
-}
-
-func decode(arr []byte) Person1 {
-	i := 0
-	person := Person1{}
-	for i < len(arr) {
-		num := arr[i]
-
-		buffType := num & 0b111
-		buffTag := num >> 3
-		fmt.Println(num, buffTag, buffType)
-
-		switch buffType {
-		case 2:
-			length := int(arr[i+1])
-			stringArr := arr[i+2 : i+2+length]
-			str := string(stringArr)
-			if i == 0 {
-				person.UserName = str
-			} else {
-				person.Interests = append(person.Interests, str)
-			}
-			fmt.Println(str)
-			i = i + 2 + length
-			break
-		case 0:
-			bit := 0
-			num := 0
-			for arr[i+1]>>7 != 0 {
-				curr := int(arr[i+1]) & 0x7F
-				num = num + curr<<bit
-
-				bit += 7
-				fmt.Println(num)
-				i++
-
-			}
-
-			curr := int(arr[i+1]) & 0x7F
-			num = num + curr<<bit
-			bit += 7
-			fmt.Println(num)
-			i = i + 2
-			person.FavoriteNumber = num
-
-			break
-
-		default:
-			//
-
-		}
-
-	}
-
-	return person
-
-}
 
 func main() {
 
-	arr := []byte{
-		0x0a, 0x06, 0x4d, 0x61, 0x72, 0x74, 0x69, 0x6e,
-		0x10, 0xb9, 0x0a, 0x1a, 0x0b, 0x64, 0x61, 0x79,
-		0x64, 0x72, 0x65, 0x61, 0x6d, 0x69, 0x6e, 0x67,
-		0x1a, 0x07, 0x68, 0x61, 0x63, 0x6b, 0x69, 0x6e,
-		0x67,
-	}
-	testPerson := decode(arr)
-	b, err := json.MarshalIndent(testPerson, "", "  ")
-	if err != nil {
-		panic(err)
+	person := endec.Person{
+		Name: "Mahboob",
+		Id:   10,
 	}
 
-	fmt.Println(string(b))
-
-	person := Person{
-		Name:  "John Doe",
-		ID:    1234,
-		Email: "john@example.com",
+	schema := endec.ProtoStruct{
+		"Person": {
+			{
+				FieldName:   "name",
+				FieldSeqNum: 1,
+				FieldType:   "string",
+			},
+			{
+				FieldName:   "id",
+				FieldSeqNum: 2,
+				FieldType:   "int32",
+			},
+		},
 	}
-	fmt.Println(en(person))
+
+	arr1 := endec.Encoder(person, schema)
+	// fmt.Println(arr1)
+	fmt.Printf("% x\n", arr1)
+
+	testPerson := endec.Decoder(arr1)
+
+	fmt.Println(testPerson)
 
 }
